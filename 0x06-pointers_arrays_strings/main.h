a4293ef2f6abfac9debf4a92ef5b3a909e1bb825
ekrika <ekrikakenny@gmail.com>
2022-10-27 20:51:34 +0000

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 int _strcmp(char *s1, char *s2);
@@ -13,5 +12,4 @@ char *rot13(char *);
 void print_number(int n);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
 #endif

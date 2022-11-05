32f61d337978a0b84f41780447da2cea36663698
KDE <ekrikakenny@gmail.com>
2022-11-05 10:09:11 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,42 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
 /**
- * main - adds positive numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: holds the number of arguements passed
+ * @argv: the array pointer that holds the arguement passed
+ * Return: always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+        int cents, coins_number = 0;
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		add += atoi(argv[i]);
-	}
-	printf("%d\n", add);
-	return (0);
+        if (argc != 2)
+        {
+        printf("Error\n");
+        return (1);
+        }
+        else
+        {
+        cents = atoi(argv[1]);
+        while (cents > 0)
+        {
+                if (cents >= 25)
+                        cents -= 25;
+                else if (cents >= 10)
+                        cents -= 10;
+                else if (cents >= 5)
+                        cents -= 5;
+                else if (cents >= 2)
+                        cents -= 2;
+                else if (cents >= 1)
+                        cents -= 1;
+                coins_number += 1;
+        }
+        }
+        printf("%d\n", coins_number);
+        return (0);
 }
+
